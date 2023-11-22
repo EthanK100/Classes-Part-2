3325c8c519a5ce76fd0bf145342ad4b9d8ef380e
EthanK100 <emklenda@gmail.com>
2023-11-22 12:07:57 -0500

    populated the truck class with code

--- Truck.cpp
+++ Truck.cpp
@@ -1,1 +1,31 @@
 #include "Truck.h"
+
+void Truck::setTowCapacity(float TC) // sets the value for the truck's tow capacity
+{
+	towCapacity = TC;
+}
+
+float Truck::getTowCapacity() // returns the value stored in towCapacity
+{
+	return towCapacity;
+}
+
+Truck::Truck(float TC, int Y, string M) // upon creation of a truck object, sets values for the member variables
+{
+	setTowCapacity(TC);
+	setYear(Y);
+	setManufacturer(M);
+}
+
+Truck::Truck() // upon creation of a truck object without parameters, sets default values for the member variables
+{
+	setTowCapacity(0);
+	setYear(0);
+	setManufacturer("unknown");
+}
+
+void Truck::displayInfo() // displays the information about the truck
+{
+	Vehicle::displayInfo();
+	cout << "Tow Capactiy: " << towCapacity << " kilograms" << endl;
+}
--- Truck.h
+++ Truck.h
@@ -5,6 +5,23 @@
 
 class Truck : public Vehicle
 {
+private:
+	float towCapacity; // truck's tow capacity in kilograms
+
+public:
+
+	//constructors
+	Truck(); // default constructor
+	Truck(float TC, int Y, string M);
+
+	//setter
+	void setTowCapacity(float TC);
+
+	//getter
+	float getTowCapacity();
+
+	//other member function
+	void displayInfo();
 
 };
 #endif
