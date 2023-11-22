62b51438e77b3a9f7a1d50d1f3397e3fc3a3591e
EthanK100 <emklenda@gmail.com>
2023-11-22 11:33:19 -0500

    populated the Vehicle class with code

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,1 +1,27 @@
 #include "Vehicle.h"
+
+void Vehicle::setYear(int Y) // sets the vehicle year
+{
+	year = Y;
+}
+
+void Vehicle::setManufacturer(string M) // sets the vehicle manufacturer
+{
+	manufacturer = M;
+}
+
+int Vehicle::getYear() // returns the value stored in year
+{
+	return year;
+}
+
+string Vehicle::getManufacturer() // returns the value stored in manufacturer
+{
+	return manufacturer;
+}
+
+Vehicle::Vehicle(int Y, string M) // constructor that sets up the member variables for the vehicle class
+{
+	Vehicle::setYear(Y);
+	Vehicle::setManufacturer(M);
+}
--- Vehicle.h
+++ Vehicle.h
@@ -7,6 +7,23 @@ using namespace std;
 
 class Vehicle
 {
+private:
+	
+	int year;
+	string manufacturer;
+	
+public:
+
+	//constructor
+	Vehicle(int Y, string M);
+	
+	//setters
+	void setYear(int Y);
+	void setManufacturer(string M);
+
+	//getters
+	int getYear();
+	string getManufacturer();
 
 };
 #endif
