537b9b6c03d5cc62534fedb5b438a8614ae54be6
EthanK100 <emklenda@gmail.com>
2023-11-22 12:29:12 -0500

    added code to main function that has the user enter information into a car object, then displays that information.

--- Source.cpp
+++ Source.cpp
@@ -26,15 +26,39 @@ int main()
 	cout << "Enter the vehicle manufacturer: ";
 	getline(cin, vehicleManufacturer);
 	vehicleObj.setManufacturer(vehicleManufacturer);
+	
 	cout << "Enter the year the vehicle was built: ";
 	cin >> vehicleYear;
 	vehicleObj.setYear(vehicleYear);
 
 	//displays the information of the vehicle
-	cout << endl << "Here is the information on the vehicle: " << endl;
+	cout << endl << "Here is the information on the vehicle:" << endl;
 	vehicleObj.displayInfo();
 	cout << endl;
+
+	// gets information from the user to store into a car object
+	Car carObj;
 	
+	cout << "Please enter information for a car below." << endl;
+	cout << "Enter the car manufacturer: ";
+	cin.ignore(1);
+	getline(cin, vehicleManufacturer);
+	carObj.setManufacturer(vehicleManufacturer);
+	
+	cout << "Enter the year the car was built: ";
+	cin >> vehicleYear;
+	carObj.setYear(vehicleYear);
+	
+	cout << "Enter the amount of doors on the car: ";
+	cin >> doors;
+	carObj.setDoors(doors);
+
+	//displays the information of the car
+	cout << endl << "Here is the information on the car:" << endl;
+	carObj.displayInfo();
+	cout << endl;
+
+
 
 	return 0;
 }
