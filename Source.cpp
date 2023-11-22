969caa17c2cb31335d57b47cebed76d49ee55ed3
EthanK100 <emklenda@gmail.com>
2023-11-22 12:16:52 -0500

    added code in the main function to get information on a vehicle object and then display the information

--- Source.cpp
+++ Source.cpp
@@ -14,7 +14,27 @@ using namespace std;
 
 int main()
 {
+	// variables to hold information on a vehicle
+	Vehicle vehicleObj;
+	int vehicleYear;
+	string vehicleManufacturer;
+	int doors;
+	float towCapacity;
+	
+	// gets information form the user to store into the vehicle object
+	cout << "Please enter information for a vehicle below." << endl;
+	cout << "Enter the vehicle manufacturer: ";
+	cin.ignore(1);
+	getline(cin, vehicleManufacturer);
+	vehicleObj.setManufacturer(vehicleManufacturer);
+	cout << "Enter the year the vehicle was built: ";
+	cin >> vehicleYear;
+	vehicleObj.setYear(vehicleYear);
 
+	//displays the information of the vehicle
+	cout << "Here is the information on the vehicle: " << endl;
+	vehicleObj.displayInfo();
+	cout << endl;
 	
 
 	return 0;
